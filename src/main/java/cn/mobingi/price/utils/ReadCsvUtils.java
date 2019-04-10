5b3e1fc6b66f859ae311d2dfb6a4ad68d97db1f0
sangchunfeng <chunfeng.sang@mobingi.cn>
2019-04-10 17:49:55 +0800

    修改方法返回值

--- src/main/java/cn/mobingi/price/utils/ReadCsvUtils.java
+++ src/main/java/cn/mobingi/price/utils/ReadCsvUtils.java
@@ -1,59 +1,39 @@
 package cn.mobingi.price.utils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ReadCsvUtils {
 
-    public static void getIndexJson() throws Exception {
+    public static Map<String,Object> getTableNameAndTableData() throws Exception {
         String indexURL = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AWSEvents/current/index.csv";
         URL url = new URL(indexURL);
         HttpURLConnection urlcon = (HttpURLConnection)url.openConnection();
         urlcon.connect();//获取连接
         InputStream is = urlcon.getInputStream();
         BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
-        StringBuffer bs = new StringBuffer();
-        String l = null;
-        String everyLine = null;
-        List<String> allString = new ArrayList<String>();
-        while((l=buffer.readLine())!=null){
-            everyLine = l;
-            allString.add(everyLine);
-        }
-        System.out.println(allString.get(0));
-    }
-
-    public static void readCscInDir() {
-        File csvDir = new File("/Users/sang/mobingi/tables"); // CSV文件路径
-        File[] fileList = null;
-        if (csvDir.exists()) {
-           fileList = csvDir.listFiles();
-        }
-        BufferedReader br = null;
-        String line = "";
-        String everyLine = "";
+        String line = null;
         List<String> allString = new ArrayList<>();
-        for (File file:fileList) {
-            file.setReadable(true);
-            file.setWritable(true);
-            try {
-                br = new BufferedReader(new FileReader(file));
-                while ((line = br.readLine()) != null) {
-                    everyLine = line;
-                    allString.add(everyLine);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        while((line = buffer.readLine())!=null){
+            allString.add(line);
         }
-        System.out.println(allString.get(0));
+        String tableName = allString.get(4).split(",")[1].toLowerCase();
+        String[] fields = allString.get(5).split(",");
+        Map<String,Object> map = new HashMap<>();
+        map.put("tableName",tableName);
+        map.put("fields",fields);
+        return map;
     }
 
     public static void main(String[] args) throws Exception {
-        ReadCsvUtils.getIndexJson();
+        ReadCsvUtils.getTableNameAndTableData();
     }
 
 }
