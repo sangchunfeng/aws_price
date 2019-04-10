2aa7b5f4a984bce247edbc4d8dc967d113535cc8
sangchunfeng <chunfeng.sang@mobingi.cn>
2019-04-10 17:36:18 +0800

    去掉多余引用

--- src/main/java/cn/mobingi/price/config/CreateTableAndInsertDataTask.java
+++ src/main/java/cn/mobingi/price/config/CreateTableAndInsertDataTask.java
@@ -5,17 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-
-import java.time.LocalDateTime;
 
 @Component
 @Configuration //1.主要用于标记配置类，兼备Component的效果。
 @EnableScheduling // 2.开启定时任务
 public class CreateTableAndInsertDataTask {
+
     @Autowired
     private AWSMapper awsMapper;
 
